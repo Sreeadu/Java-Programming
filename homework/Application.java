82b525b8e282d1fd55a6a062336283a87c2ccb94
Sreeadu <sree.had@gmail.com>
2017-08-29 17:10:22 -0700

    Add files via upload

---
+++ homework/Application.java
@@ -0,0 +1,25 @@
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		int age = 20; 
+		
+		if (age < 13) {
+				System.out.println("You are kid");
+				
+		} else if(age < 20) {
+				
+				System.out.println("You are teen");
+	}
+		 else if(age < 61){
+			 System.out.println("You are adult");
+			 
+		 }
+		 else {
+			 System.out.println("You are senoir citizen");
+		 }
+	}
+	
+}
