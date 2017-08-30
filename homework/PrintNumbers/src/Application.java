7f52de93a0c32989e047b9a8b070f675b5c98522
Sreeadu <sree.had@gmail.com>
2017-08-29 17:41:40 -0700

    Create Application.java

---
+++ homework/PrintNumbers/src/Application.java
@@ -0,0 +1,15 @@
+public class Application {
+
+	public static void main(String[] args) {
+		int x;
+		for (x = 5 ; x<100 ; x = x+10) {
+			
+			if (x == 95) {
+				System.out.println(x);
+			} else {
+				System.out.print(x + ", ");
+			}
+		}
+	}
+
+}
